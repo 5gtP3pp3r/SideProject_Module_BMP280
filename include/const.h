5ff0cb770525c672b516099ca010909c0aeeafa2
5gtp3pp3r <william.crepault@gmail.com>
2025-01-18 00:32:54 -0500

    Fichiers const.h Harmonisé à tout les projets... pour l'instant

--- include/const.h
+++ include/const.h
@@ -7,14 +7,34 @@
 #define EMPTY ""
 #define DEFAULT_ZERO 0
 #define DEFAULT_ZERO_F 0.0f
-#define INTERVAL_5 5000
+#define INTERVAL_5 5000                 // 5 secondes
 #define INTERVAL_4 4000
 #define INTERVAL_3 3000
 #define INTERVAL_2 2000
 #define INTERVAL_1 1000
-#define INTERVAL_50 50
+#define INTERVAL_500 500                // 500 millisecondes
+#define INTERVAL_400 400
+#define INTERVAL_300 300
+#define INTERVAL_200 200
+#define INTERVAL_100 100
+#define INTERVAL_50 50                  // 50 millisecondes
 #define INTERVAL_40 40
 #define INTERVAL_30 30
-#define INTERVAL_500 500
+#define INTERVAL_20 20
+#define INTERVAL_10 10
+
+// BMP280
+#define BMP_ADD 0x76                    // SD0 sur GND. Si VCC: 0x77
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define DEGRE "°c"
+#define METRE "m"
+#define KPA "kPa"
+#define PERCENT "%" 
+#define KILOPASCAL 1000.0f
+
+// ADXL345
+#define ADXL_ADD 0x53                   // SD0 sur GND. SI sur VCC: 0x1D
+#define MS2 "m/s²"                      // Accélération en mètre par seconde au carré. La gravité est une accélération constante de 9.81m/s² soit 1G.
+#define G_CONST 9.81f                   // Constante De la gravité.
 
 #define JSON_FILE_PATH "/config.json"
