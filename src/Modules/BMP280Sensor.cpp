5de1de4998a3d37f3a8d30be400847bfbdeafb77
5gtp3pp3r <william.crepault@gmail.com>
2025-01-18 15:47:58 -0500

    Mise à jours commentaires BMP280

--- src/Modules/BMP280Sensor.cpp
+++ src/Modules/BMP280Sensor.cpp
@@ -4,9 +4,9 @@
 
 BMP280Sensor::BMP280Sensor()
   : m_temperature(DEFAULT_ZERO_F),
-    m_altitude(DEFAULT_ZERO_F),                             // Pour afficher un altitude réel depuis le sol du lancement, devra créer une classe pour le clacul. 
-    m_presure(DEFAULT_ZERO_F),                              // Prendre l'altitude stable avant le lancement, le conserver dans une données membre et relancer 
-    m_lastReadTime(DEFAULT_ZERO),                           // l'altitude moins la donnée enregistrée. Comme ça l'altitude depuis le sol sera visible. 
+    m_altitude(DEFAULT_ZERO_F),                             // Pour afficher un altitude réel depuis le sol du lancement, devra créer une classe pour le clacul.  Prendre l'altitude
+    m_presure(DEFAULT_ZERO_F),                              // stable avant le lancement, la conserver dans une données membre et relancer l'altitude moins la donnée enregistrée. 
+    m_lastReadTime(DEFAULT_ZERO),                           // Comme ça l'altitude depuis le sol sera visible. Ou bien dans une donnée membre à l'intérieur de cette classe directement.
     m_interval(INTERVAL_500) {                              // Pour l'instant, lecture au 1/2 seconde.          
         if (!m_BMP280.begin(BMP_ADD)) {                     // Simple vérification affiché dans le port série. A servi à détecter que je n'avais pas un BME mais un BMP.
             Serial.println("Incapable de démarrer le capteur BME280. Vérifiez l'adresse et le câblage.");
